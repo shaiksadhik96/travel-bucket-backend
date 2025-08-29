3f4a6d7872a1b01644e0c0293617022484a87e6e
Shaik Sadhik <sadhikshaik9618@gmail.com>
2025-08-29 12:17:15 +0530

    issues resolved
    issues resolved

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+  // Remove favicon.ico 404 error
+  app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // Routes
 const placeRoutes = require('./routes/placeRoutes');
 app.use('/api/places', placeRoutes);
-
+app.get('/', (req, res) => res.send('Travel Wishlist API'));
 // Connect MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
