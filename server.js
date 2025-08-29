31b29946d0b46de47c5e529922f2f3756d0ffd1f
Shaik Sadhik <sadhikshaik9618@gmail.com>
2025-08-29 14:18:36 +0530

    added cors
    added cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors()); // Allows all origins
 app.use(express.json());
   // Remove favicon.ico 404 error
   app.get('/favicon.ico', (req, res) => res.status(204).end());
